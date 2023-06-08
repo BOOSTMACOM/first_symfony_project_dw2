8447a0e1b1fdd712ec3eb6e7e2cb787c0074d303
GamorySensei <theo.gamory@metznumericschool.fr>
2023-06-08 15:02:18 +0200

    Début du crud pour les Post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\EditPostFormType;
 use App\Repository\PostRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
@@ -16,6 +19,29 @@ class PostController extends AbstractController
 
         return $this->render('post/index.html.twig', [
             'posts' => $posts,
+        ]);
+    }
+
+    #[Route('/post/new', name: 'app_post_new')]
+    public function new(
+        Request $request,
+        PostRepository $postRepository
+    ) {
+        $form = $this->createForm(EditPostFormType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted())
+        {
+           $post = $form->getData();
+           $post->setCreatedAt(new \DateTimeImmutable());
+
+           $postRepository->save($post, true);
+
+           return $this->redirectToRoute('app_post');
+        }
+
+        return $this->render('post/new.html.twig', [
+            'editPostForm' => $form->createView()
         ]);
     }
 }
